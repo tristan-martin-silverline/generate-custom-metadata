c8e474cc10311aa25ec53956226daac9708093ad
tristanmartin <tristan.martin@silverlinecrm.com>
2021-10-19 15:41:50 -0500

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+## Generate Custom Metadata
+#### Table of Contents
+- [Setup](#setup)
+- [How To Use It?](#how-to-use-it)
+  - [Create The CSV](#create-the-csv)
+  - [Run The Command](#run-the-command)
+- [Example](#example)
+### Setup
+1. If not already installed, download and install NodeJS from [here](https://nodejs.org/en/download/)
+2. Clone or download the repository
+3. Navigate to the root of the repository
+4. Run `npm install` to download dependencies
+### How To Use It?
+#### Create The CSV
+The CSV file should be generated so that one column represents the label of the Custom Metadata record and the other columns represent the API name of the fields to map to.
+
+| Label | ApiName1__c | ApiName2__c | ... | ApiNameN__c |
+|-------|-------------|-------------|-----|-------------|
+| Test1 | Value1      | Value2      | ... | ValueN      |
+| Test2 | Value1      | Value2      | ... | ValueN      |
+#### Run The Command
+```
+USAGE
+  $ node metadata-creator.js -f <filepath> -l <string> -t <string> [-o <filepath>]
+OPTIONS
+  -f, --file                   path to csv file to load
+  
+  -h, --help                   command reference
+  
+  -l, --label                  column header in csv that represents 
+                               the label of the metadata type
+                               
+  -o, --output-dir             path to output directory
+  
+  -t, --metadata-type          api name of the metadata type to use
+```
+### Example
+**_Test.csv_** is included as an example in the repo. To run, execute the following command...
+
+`node metadata-creator.js -f test.csv -l Label -t Test__mdt`
---
+++ metadata-creator.js
@@ -0,0 +1,94 @@
+const { program } = require('commander');
+const csv = require('csvtojson');
+const convert = require('xml-js');
+const fs = require('fs');
+
+main();
+
+async function main() {
+    program
+        .version('0.0.1')
+        .requiredOption('-f, --file <file>', 'name of the csv file')
+        .requiredOption('-l, --label <label>', 'the field in the csv file that represents the label of the custom metadata')
+        .requiredOption('-t, --metadata-type <type>', 'the api name of the metadata type to generate')
+        .option('-o, --output-directory <directory>', 'the directory to output the generated Custom Metadata files', './output')
+        .parse(process.argv);
+
+    const args = program.opts();
+
+    const path = __dirname + '/' + args.file;
+    const rows = await csv().fromFile(path);
+
+    const metadataFields = [];
+    for (const key of Object.keys(rows[0])) {
+        if (key !== args.label) metadataFields.push(key);
+    }
+
+    createMetadataFiles(args.metadataType, args.label, metadataFields, rows, args.outputDirectory);
+}
+
+function createMetadataFiles(type, label, metadataFields, rows, outputDirectory) {
+    for (const row of rows) {
+        const values = [];
+        for (let i = 0; i < metadataFields.length; i++) {
+            const isPopulated = !!row[metadataFields[i]];
+            values.push({
+                field: metadataFields[i],
+                value: {
+                    '_attributes': {
+                        ...isPopulated && {'xsi:type': 'xsd:string'},
+                        ...!isPopulated && {'xsi:nil': 'true'}
+                    },
+                    ...isPopulated && {'_text': row[metadataFields[i]]}
+                }
+            });
+        }
+        const metadataXML = {
+            '_declaration': {
+                '_attributes': {
+                    'version': '1.0',
+                    'encoding': 'UTF-8'
+                }
+            },
+            CustomMetadata: {
+                '_attributes': {
+                    'xmlns': 'http://soap.sforce.com/2006/04/metadata',
+                    'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+                    'xmlns:xsd': 'http://www.w3.org/2001/XMLSchema'
+                },
+                label: row[label].substr(0, 40),
+                protected: false,
+                values
+            }
+        };
+        const options = {compact: true, ignoreComment: true, spaces: 4};
+        const xml = convert.js2xml(metadataXML, options) + '\n';
+        const fileName = type + '.' + createValidFileName(row[label].substr(0, 40), '.md-meta.xml');
+        writeXMLFile(outputDirectory, fileName, xml);
+    }
+}
+
+function writeXMLFile(directory, fileName, xml) {
+    const dirPath = __dirname + (directory ? '/' + stripTrailingSlash(directory) : '');
+    if (!fs.existsSync(dirPath)){
+        fs.mkdirSync(dirPath);
+    }
+
+    const path = dirPath + '/' + fileName;
+    fs.writeFileSync(path, xml);
+}
+
+function createValidFileName(name, extension) {
+    name = !isNaN(name.charAt(0)) ? 'X' + name.replace(/[^0-9a-z]+/gi, '_') : name.replace(/[^0-9a-z]+/gi, '_');
+    if(name[name.length - 1] === "_") {
+        name = name.substr(0, name.length - 1);
+    }
+    return name + extension;
+}
+
+function stripTrailingSlash(str) {
+    if(str.substr(-1) === '/') {
+        return str.substr(0, str.length - 1);
+    }
+    return str;
+}
